4aa06e1be3e9a0481c1d01c7fdd27e41970a4dfd
Gautam <gautam.kumar@tier5.in>
2022-05-18 00:36:34 +0530

    portfolio v1.0

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+import Navabar from './Navbar'
+import MasterHeadSection from './MasterHeadSection'
+import PortfolioSection  from './PortfolioSection'
+import ContactSection from './ContactSection'
+import Footer from './Footer'
+import Style from './styles.css'
+
+const App = () => (
+  <>
+    <Navabar/>
+    <MasterHeadSection/>
+    <PortfolioSection/>
+    <ContactSection/>
+    <Footer/>
+  </>
+)
+
+
+
+export default App
